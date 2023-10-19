4c9186f5e40ffd358ee02cf4ad9bab126f6060a2
vicmalpat <vicky-vera-bibi@hotmail.com>
2023-10-18 23:37:17 -0600

    actualización2.0

---
+++ src/main/java/com/example/restaurante/pruebas/Pruebas.java
@@ -0,0 +1,41 @@
+package com.example.restaurante.pruebas;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static com.example.restaurante.modelo.Conexion.conexion;
+
+public class Pruebas {
+    public boolean guardarImagen(String ruta,String nombre){
+        String insert = "insert into Imagenes(imagen,nombre) values(?,?)";
+        FileInputStream fis = null;
+        PreparedStatement ps = null;
+        try {
+            conexion.setAutoCommit(false);
+            File file = new File(ruta);
+            fis = new FileInputStream(file);
+            ps = conexion.prepareStatement(insert);
+            ps.setBinaryStream(1,fis,(int)file.length());
+            ps.setString(2, nombre);
+            ps.executeUpdate();
+            conexion.commit();
+            return true;
+        } catch (Exception ex) {
+            Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            try {
+                ps.close();
+                fis.close();
+            } catch (Exception ex) {
+                Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/example/restaurante/pruebas/im.java
@@ -0,0 +1,26 @@
+package com.example.restaurante.pruebas;
+
+import javafx.scene.image.Image;
+
+public class im {
+    public class Imagen {
+        Image imagen;
+        String nombre;
+
+        public void setImagen(Image imagen) {
+            this.imagen = imagen;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public Image getImagen() {
+            return imagen;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+    }
+}
