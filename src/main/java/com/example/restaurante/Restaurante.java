9a99579d7ab1aaa26e3f6c15a97dcffb40c2a6ad
gusramireles46 <gusramireles@outlook.com>
2023-10-25 05:29:48 -0600

    Actualización título de la ventana

--- src/main/java/com/example/restaurante/Restaurante.java
+++ src/main/java/com/example/restaurante/Restaurante.java
@@ -33,7 +33,7 @@ public class Restaurante extends Application {
         CrearGUI();
         Scene scene = new Scene(bdpPrincipal, 1280, 720);
         scene.getStylesheets().add(getClass().getResource("/css/estilos_principal.css").toExternalForm());
-        stage.setTitle("Cola de Gato");
+        stage.setTitle("Restaurante Cola de Gato");
         stage.setScene(scene);
         stage.show();
     }
