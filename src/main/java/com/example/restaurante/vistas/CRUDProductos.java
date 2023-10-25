ae60b83569a5b75d4ce369f9df68b64fcf88cf8b
gusramireles46 <gusramireles@outlook.com>
2023-10-24 20:34:38 -0600

    Actualización de CRUDProductos

--- src/main/java/com/example/restaurante/vistas/CRUDProductos.java
+++ src/main/java/com/example/restaurante/vistas/CRUDProductos.java
@@ -1,4 +1,29 @@
 package com.example.restaurante.vistas;
 
-public class CRUDProductos {
+import com.example.restaurante.modelo.ProductosDAO;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class CRUDProductos extends Stage {
+    private ProductosDAO productosDAO;
+    private VBox vbox;
+    private TableView<ProductosDAO> tbvProductos;
+    private Button btnAgregar;
+    private Scene escena;
+
+    public CRUDProductos() {
+        crearGUI();
+        this.setTitle("CRUD de Productos");
+        this.setScene(escena);
+        this.sizeToScene();
+        this.show();
+    }
+
+    private void crearGUI() {
+        productosDAO = new ProductosDAO();
+    }
+
 }
